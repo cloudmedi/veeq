0a77ef0d2958b1487eb3773d6f83985c070de870
dlarashn <alelislay@gmail.com>
2024-02-02 08:56:04 +0300

    Asset changes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,12 +2,14 @@
 import Hero from "@/components/hero";
 import Zigzag from "@/components/zigzag";
 import Pricing from "@/components/pricing";
+import Features from "@/components/features";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Zigzag />
+      <Features />
       <Pricing />
     </>
   );
