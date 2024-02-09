edb26673c96a77276d4231e2d9aab14c4c7abcd6
berkayakgurgen <akgurgennberkay@gmail.com>
2024-02-09 14:39:04 +0300

    Remove component

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,14 +3,12 @@ import Hero from "@/components/hero";
 import Zigzag from "@/components/zigzag";
 import Pricing from "@/components/pricing";
 import Features from "@/components/features";
-import SampleMusic from "@/components/sampleMusics";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Zigzag />
-      <SampleMusic />
       <Features />
       <Pricing />
     </>
