3ebbe96846481b0cca4854fd70eb120fa489a116
Berkay Akgurgen <akgurgennberkay@gmail.com>
2024-02-13 09:58:42 +0300

    404 page route fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
-
 export default function NotFound() {
   return (
     <div className="global w-screen h-screen flex flex-col justify-center items-center">
       <h2 className={"mb-4 text-4xl tracking-tight font-extrabold text-white"}>
         404
       </h2>
-      <p>Could not find requested resource</p>
-      <Link className={"underline"} href={"/"}>
-        Click to redirect home page
-      </Link>
+      <p>Could not find requested resource.</p>
     </div>
   );
 }
