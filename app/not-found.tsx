21b8639c17d4f3d15351e9c3c50e8ce2d7b8406d
Berkay Akgurgen <akgurgennberkay@gmail.com>
2024-02-13 09:50:48 +0300

    404 page fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,15 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function NotFound() {
-  const router = useRouter();
-
-  useEffect(() => {
-    let timer = null;
-
-    timer = setTimeout(() => {
-      router.push(`/library`);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="global w-screen h-screen flex flex-col justify-center items-center">
       <h2 className={"mb-4 text-4xl tracking-tight font-extrabold text-white"}>
         404
       </h2>
       <p>Could not find requested resource</p>
-      <p>You'll redirect to home page.</p>
+      <Link className={"underline"} href={"/"}>
+        Click to redirect home page
+      </Link>
     </div>
   );
 }
